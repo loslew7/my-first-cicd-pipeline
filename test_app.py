b17a5563024fcd88e4b1f890f076bba08302aaa1
loslew7 <loslew7@gmail.com>
2026-06-10 10:30:09 -0500

    Break a test intentionally
    Correct the expected result for adding -1 and 1.

--- test_app.py
+++ test_app.py
@@ -10,7 +10,7 @@ def test_add():
     """Test the add function"""
     assert add(2, 3) == 5
     assert add(0, 0) == 0
-    assert add(-1, 1) == 0
+    assert add(-1, 1) == 10 # WRONG! Should be 0
 
 def test_divide():
     """Test the divide function"""
